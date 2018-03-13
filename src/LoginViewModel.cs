ba73e841ac700f8be98dd981c69ec8625e594f00
sKanoodle <sKanoodle@users.noreply.github.com>
2018-03-13 17:29:06 +0100

    first working example, displays torrents in datagrid

---
+++ src/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Transmission.Client
+{
+    class LoginViewModel
+    {
+        public string Address { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public ICommand Close => new RelayCommand(o => ((Window)o).Close());
+    }
+}
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Transmission.Api.Entities;
 
 namespace Transmission.Client
 {
@@ -20,9 +22,39 @@ namespace Transmission.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Torrent> Torrents { get; } = new ObservableCollection<Torrent>();
+
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+            DoStuffAsync();
+        }
+
+        LoginViewModel LoginVM = new LoginViewModel();
+
+        private async Task DoStuffAsync()
+        {
+            var window = new Window();
+            window.Content = LoginVM;
+            window.ShowDialog();
+
+            Api.Client client = new Api.Client(LoginVM.Address, LoginVM.Username, LoginVM.Password);
+            while (true)
+            {
+                await UpdateShit(client);
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task UpdateShit(Api.Client client)
+        {
+            var result = await client.TorrentGetAsync(TorrentFields.Name | TorrentFields.PercentDone | TorrentFields.RateDownload | TorrentFields.RateUpload);
+            foreach (var torrent in result)
+            {
+                Torrents.Remove(Torrents.Where(t => t.Name == torrent.Name).SingleOrDefault());
+                Torrents.Add(torrent);
+            }
         }
     }
 }
